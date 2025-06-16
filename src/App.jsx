296953b4b6eae3d8a00381180763e56c281936f6
Jhon Joshua Abutan <sjosh2503@gmail.com>
2025-06-16 23:16:37 +0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Hi, I'm Jhon Joshua Abutan</h1>
+        <p className="mt-2 text-lg">Frontend developer. I love designing websites and turning them into code. Slightly addicted to coffee and not a fan of CSS.</p>
+      </div>
+    </div>
+  );
+}
